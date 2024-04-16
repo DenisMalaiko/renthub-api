97cd483a83c1a05f1771008ecbb063be6463526f
denysmalaiko <malaiko.denis@gmail.com>
2024-04-16 20:13:00 +0300

    Init GraphQl

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoClient = require("mongodb").MongoClient;
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 const app = express();
 
@@ -11,12 +13,40 @@ app.use(cors());
 
 const posts = require('./routes/api/posts');
 
+
+app.use("/graphql", graphqlHTTP({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+        
+        type RootMutation {
+            createEvent(name: String): String
+        }
+        
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return ["1", "2", "3"]
+        },
+        createEvent: (args) => {
+            const eventName = args.name;
+            return eventName;
+        }
+    },
+    graphiql: true
+}))
+
 app.use('/api/posts', posts);
 
 const port = process.env.PORT || 3000;
 
-const CONNECTION_STRING = "mongodb+srv://DenisMalaiko:PDZqTQidkRfcAljb@renthub.de39wzv.mongodb.net/?retryWrites=true&w=majority&appName=renthub";
-const DATABASE = "renthub";
+/*const CONNECTION_STRING = "mongodb+srv://DenisMalaiko:PDZqTQidkRfcAljb@renthub.de39wzv.mongodb.net/?retryWrites=true&w=majority&appName=renthub";
+const DATABASE = "renthub";*/
 
 app.listen(port, () => {
     console.log(`Server ${port}`);
